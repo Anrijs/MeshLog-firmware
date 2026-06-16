5e753919b63f33510d18bacb9a366e8cf73ad7fd
Anrijs <anrijs@anrijs.lv>
2026-06-16 21:08:26 +0300

    v1.5.0

--- src/meshlog/version.h
+++ src/meshlog/version.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define FIRMWARE_VER_TEXT "v1.16.0"
-#define LOGGER_VER_TEXT "v1.4.0"
+#define LOGGER_VER_TEXT "v1.5.0"
 #define BUILD_DATE __DATE__ " " __TIME__
 #define FULL_VER_TEXT "fw: " FIRMWARE_VER_TEXT ", logger: " LOGGER_VER_TEXT ", build-time: " BUILD_DATE
